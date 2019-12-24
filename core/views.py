a406a4f833c386169fb2abb98f6db4061a84452b
Rafael Borges <rafaelfborges@gmail.com>
2019-12-24 12:55:41 -0300

    Incluído template para cadastrar eventos na agenda.

--- core/views.py
+++ core/views.py
@@ -3,12 +3,8 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
 # Create your views here.
-
-def logout_user(request):
-    logout(request)
-    return redirect('/')
-
 def submit_login(request):
     if request.POST:
         username = request.POST.get('username')
@@ -24,9 +20,30 @@ def submit_login(request):
 def login_user(request):
     return render(request, 'login.html')
 
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
     data = {'eventos':evento}
     return render(request, 'agenda.html', data)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
